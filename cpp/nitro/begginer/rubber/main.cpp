1804b34b3e56b61bdd9c77e512f639eae49811f2
costa <devbcdc@proton.me>
2024-03-08 22:30:35 -0300

    feat: add the rubber solution

---
+++ cpp/nitro/begginer/rubber/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rubber, volume, diff;
+    cin >> rubber;
+    cin >> volume;
+    diff = rubber-volume;
+    cout << diff << endl;
+    return 0;
+}
